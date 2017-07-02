26fbb34405d0626b2e53c531b54ab55d4f96934e
Lucas deGomez <lucasdegomez@gmail.com>
2017-07-02 19:43:34 +0200

    Redefines styling and header html

--- app/components/app-component/app-component.js
+++ app/components/app-component/app-component.js
@@ -3,6 +3,8 @@ import template from './app-component-template.html';
 import getInfo from '../../services/api'
 import {MediaPlayer} from 'dashjs';
 import vueScrollto from 'vue-scrollto'
+import moment from 'moment'
+import duration from 'moment-duration-format'
 const mainConfig = require('../../config/config.json')
 
 Vue.use(vueScrollto)
@@ -19,10 +21,16 @@ const AppComponent = Vue.extend({
   },
   computed: {
     backdropUrl() {
-      return this.config.images.base_url + this.config.images.backdrop_sizes[2] + this.info.backdrop_path
+      return this.config.images.base_url + this.config.images.backdrop_sizes[3] + this.info.backdrop_path
     },
     posterUrl() {
-      return this.config.images.base_url + this.config.images.poster_sizes[2] + this.info.poster_path
+      return this.config.images.base_url + this.config.images.poster_sizes[3] + this.info.poster_path
+    },
+    releaseYear() {
+      return moment(this.info.release_date).year()
+    },
+    hoursAndMin() {
+      return moment.duration(this.info.runtime, "minutes").format("h[h] m[m]");
     }
   },
 
@@ -34,7 +42,6 @@ const AppComponent = Vue.extend({
       }
       const player = MediaPlayer().create();
       player.initialize(this.$refs.videoElement, url, false)
-      // player.attachVideoContainer(this.$refs.videoContainer)
       player.attachTTMLRenderingDiv(this.$refs.videoCaption)
     }
   },
